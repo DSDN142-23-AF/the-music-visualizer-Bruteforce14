a46d0e14d417eccd7a7ff1fa1c72adb66a479cd2
Bruteforce14 <fergal120302@gmail.com>
2024-02-07 15:11:09 +1300

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -1,8 +1,20 @@
-
+let firstRun = true;
+let land;
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
-  textFont('Verdana'); // please use CSS safe fonts
+ if (firstRun){
+  rectMode(CENTER);
+  land = loadImage('Newfoundland and Labrador.png');
+
+  firstRun = false
+
+ }
+ 
+  background(87, 196, 255);
+  fill(77, 70, 54);
+  rect(0,3*height/4,width*2,height/4);
+  image(land,0,100);
+  textFont('Lobster'); // please use CSS safe fonts
   rectMode(CENTER)
   textSize(24);
 
@@ -12,26 +24,29 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
  
 
    // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, width, bar_height+2*drum);
+   stroke(255);
+   strokeWeight(drum/10);
+   fill(0, 98, 122);
+   rect(bar_pos_x-5, height / 2 + 3 * bar_spacing, width+50, bar_height+2*drum);
   
  
    // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, width, bar_height+2*bass);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
+   strokeWeight(bass/10);
+   fill(1, 68, 84);
+   rect(bar_pos_x-5, height / 2 + 4 * bar_spacing, width+50, bar_height+2*bass);
+
  
    // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
+   strokeWeight(other/10);
+   fill(0, 35, 43);
+   rect(bar_pos_x-5, height / 2 + 5 * bar_spacing, width+50, bar_height+2*other);
+
  
    // display "words"
+   strokeWeight(vocal/10);
    textAlign(CENTER);
    textSize(vocal);
+   textFont('Lobster');
    text(words, width/2, height/3);
 }
 
