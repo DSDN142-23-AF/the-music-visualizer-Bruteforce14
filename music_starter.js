72f7376996c4d8f3a46a52851a943089da394583
Bruteforce14 <fergal120302@gmail.com>
2024-02-11 23:06:07 +1300

    Update music_starter.js

--- music_starter.js
+++ music_starter.js
@@ -1,31 +1,42 @@
 let firstRun = true;
 let land;
+let sky;
+let font;
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
  if (firstRun){
   rectMode(CENTER);
-  land = loadImage('Newfoundland and Labrador.png');
-
+  land = loadImage('Newfoundland and Labrador.png'); // some coast line base on newfoundland and labrador
+  sky = loadImage('Skybox.png'); // a gradient that represents the sunrise and sunset
+  font = loadFont('Shrikhand-Regular.ttf')
   firstRun = false
 
  }
+ let skyY = map(counter,0,song.duration(),0,4800) //moves the sky up so at the start of the song the sun rises and at the end it sets
+ let eigth = song.duration()/8; // 1/8 the song duration for sunrise sun set
+
+ let skyF = 255
+ if (counter<eigth) {
+   skyF = map(counter,0,eigth,0,255) // fades the light in from dark in the 1st 1/8th
+ };
+ if (counter>eigth*7){
+   skyF=map(counter,eigth*7,song.duration(),255,0) // fades the light out to dark in the last 1/8th
+};
  
-  background(87, 196, 255);
-  fill(77, 70, 54);
-  rect(0,600,width*2,height/4);
+  background(0);
+  fill(77, 70, 54); // a brown colour that matches my drawn land
+  image(sky,0,0-skyY);
+  rect(0,600,width*2,height/4); //covers the bottom of the land 
   image(land,0,0);
-  textFont('Lobster'); // please use CSS safe fonts
+  textFont(font);
   rectMode(CENTER)
   textSize(24);
 
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
  
-   // other bar is white
+   // other wave is the lightest blue close to the land
    let otherY = 500;
-   let otherYP = otherY + other;
-   let otherYN = otherY - other;
+   let otherYP = otherY + other; // so half the crests move down with the beat
+   let otherYN = otherY - other; // so half the crests move up with the beat
    strokeWeight(other/10);
    fill(0, 98, 122);
    beginShape();
@@ -50,15 +61,26 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
    curveVertex(1600,otherYP);
    curveVertex(1650,otherYP);
    curveVertex(1650,700);
-   endShape(CLOSE);
+   endShape(CLOSE); 
+
+   if (other>75 && other<90){ // creates a little droplet at the crest of the high wave if the bass is between 75 and 90
+      drawSpray(100,otherYN-20);
+      drawSpray(300,otherYN-20);
+      drawSpray(500,otherYN-20);
+      drawSpray(700,otherYN-20);
+      drawSpray(900,otherYN-20);
+      drawSpray(1100,otherYN-20);
+      drawSpray(1300,otherYN-20);
+      drawSpray(1500,otherYN-20);
+
+   } 
 
    // bass bar is blue
    let bassY = 600;
-   let bassYP = bassY - bass;
-   let bassYN = bassY + bass;
+   let bassYP = bassY - bass; // so half the crests move up with the beat
+   let bassYN = bassY + bass; // so half the crests move down with the beat
    strokeWeight(bass/10);
    fill(1, 68, 84);
-   rect(bar_pos_x-5, height / 2 + 4 * bar_spacing, width+50, bar_height+2*bass);
    beginShape();
    curveVertex(-50,800);
    curveVertex(-50,bassYP);
@@ -82,13 +104,22 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
    curveVertex(1650,800);
    endShape(CLOSE);
     
-   
+   if (bass>65 && bass<90){ // creates a little droplet at the crest of the high wave if the bass is between 65 and 90
+      drawSpray(175,bassYP-20);
+      drawSpray(375,bassYP-20);
+      drawSpray(575,bassYP-20);
+      drawSpray(775,bassYP-20);
+      drawSpray(975,bassYP-20);
+      drawSpray(1175,bassYP-20);
+      drawSpray(1375,bassYP-20);
+      drawSpray(1575,bassYP-20);
+   }
    
    // drum bar is green
    let drumY = 700;
    let drumS = drum;
-   let drumYP = drumY + drum;
-   let drumYN = drumY - drumS;
+   let drumYP = drumY + drum; // so half the crests move down with the beat
+   let drumYN = drumY - drumS; // so half the crests move up with the beat
    stroke(255);
    strokeWeight(drum/10);
    fill(0, 35, 43);
@@ -116,30 +147,73 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
    curveVertex(1650,900);
    endShape(CLOSE);
 
+   if (drum>60 && drum<90){ // creates a little droplet at the crest of the high wave if the bass is between 60 and 90
+      drawSpray(100,drumYN-20);
+      drawSpray(300,drumYN-20);
+      drawSpray(500,drumYN-20);
+      drawSpray(700,drumYN-20);
+      drawSpray(900,drumYN-20);
+      drawSpray(1100,drumYN-20);
+      drawSpray(1300,drumYN-20);
+      drawSpray(1500,drumYN-20);
+
+   } 
  
    // display "words"
+   stroke(255);
+   fill(0, 35, 43);
    strokeWeight(vocal/10);
    textAlign(CENTER);
    textSize(vocal);
-   textFont('Lobster');
+   textFont(font);
    text(words, width/2, height/5);
+   fill(0,0,26,255-skyF);
+   strokeWeight(noStroke)
+   rect(width/2,height/2,width,height)
 }
 
-function drawSpray(x,y,inst){
-   let morphdropY = []
+function drawSpray(x,y){ // creates a little droplet to be placed at wave crests
+let dropStartY=y; // was supposed to make the droplets move but didnt. didnt have time to remove as the variable is used now
+let dropStartX=x;
+
+   let morphDropY = 0;
    for(let i = 0; i <=dropStartY.length; i++ ){
-   morphdropY[i] = map(6,  0 , 1, y+1, y+50)
+   morphDropY = map(6,  0 , 1, y+1, y+50)
    }
-   let morphDropX = []
+   let morphDropX = 0;
    for(let i = 0; i <=dropStartX.length; i++ ){
-   morphDropX[i] = map(6,  0 , 1,x+1,x+10)
+   morphDropX = map(6,  0 , 1,x+1,x+10)
+   }
+strokeWeight(1);
 stroke(255);
 fill(255);
 beginShape();
-curveVertex();
-curveVertex();
-curveVertex();
-curveVertex();
+curveVertex(x+morphDropX,y+morphDropY+16);
+curveVertex(x+morphDropX-2,y+morphDropY+14);
+curveVertex(x+morphDropX-6,y+morphDropY-9);
+curveVertex(x+morphDropX,y+morphDropY-16);
+curveVertex(x+morphDropX+6,y+morphDropY-9);
+curveVertex(x+morphDropX+2,y+morphDropY+14);
+curveVertex(x+morphDropX,y+morphDropY+16);
+endShape(CLOSE);
+
+beginShape();
+curveVertex(x+morphDropX-4,y+morphDropY+17);
+curveVertex(x+morphDropX-6,y+morphDropY+16.5);
+curveVertex(x+morphDropX-20,y+morphDropY+4);
+curveVertex(x+morphDropX-20,y+morphDropY-3.5);
+curveVertex(x+morphDropX-13.4,y+morphDropY-2.4);
+curveVertex(x+morphDropX-3.5,y+morphDropY+15);
+curveVertex(x+morphDropX-4,y+morphDropY+17);
+endShape(CLOSE);
+
+beginShape();
+curveVertex(x+morphDropX+4,y+morphDropY+16.5);
+curveVertex(x+morphDropX+3.5,y+morphDropY+14);
+curveVertex(x+morphDropX+13,y+morphDropY-4.5);
+curveVertex(x+morphDropX+21,y+morphDropY-6);
+curveVertex(x+morphDropX+22,y+morphDropY+1.5);
+curveVertex(x+morphDropX+6,y+morphDropY+16);
+curveVertex(x+morphDropX+4,y+morphDropY+16.5);
 endShape(CLOSE);
    }
-}
